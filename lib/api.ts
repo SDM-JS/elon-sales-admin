037da0be1c519cbcaf887f0ff62e679895f6190f
NETWORK-7 <najmiddinme@inbox.ru>
2026-05-30 13:53:58 +0500

    Apini Linkini togriladim

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE_URL = "https://saledos-vkwu.onrender.com/api"
+const API_BASE_URL = "https://saledos-1.onrender.com/api"
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
