510601134e3229e5bba6a8a82ab4c5b3125e2688
Daler <dalerjonsodikov27@gmail.com>
2026-06-20 05:55:33 +0500

    Fix

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -14,13 +14,7 @@ import {
 } from "@/lib/api";
 import { CategoriesTable } from "@/components/categories/categories-table";
 import { CategoryDialogs } from "@/components/categories/category-dialogs";
-
-
-interface Category {
-  id: string;
-  name: string;
-  createdAt: string;
-}
+import { Category } from "@/components/categories/types";
 
 
 export default function CategoriesPage() {
