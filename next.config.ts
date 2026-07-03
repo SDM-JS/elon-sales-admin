9aae9be9ab8c8ba84608388a5fcf72f8423d9aac
Sodikov Daler <dalerjonsodikov27@gmail.com>
2026-07-03 17:55:28 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   turbopack: {
     root: __dirname,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/array/:path*",
-        destination: "https://us-assets.i.posthog.com/array/:path*",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://us.i.posthog.com/:path*",
-      },
-    ];
-  },
-=======
->>>>>>> b538eb3dcfd2330639bce82ed1cc4940d5f95cf2
   skipTrailingSlashRedirect: true,
 };
 
